3467b4ad6d4bdaeaaa8c4edc60f24584fac99bbd
Sariel <s-a-z47@mail.ru>
2017-10-22 03:30:22 +0600

    ккк
    ккк

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -19,10 +19,7 @@ const std::string CLIENT_NAME("Sariel");
  * Client version number
  */
 #define CLIENT_VERSION_SUFFIX ""
-#define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 0
-#define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 1
+
 
 
 /**
